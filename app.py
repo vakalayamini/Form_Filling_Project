394f1d1ced85c0ab3f2de3c8537ba67b34ab7753
yaminivakala <118597488+vakalayamini@users.noreply.github.com>
2024-12-28 18:54:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def form_filling():
     return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
